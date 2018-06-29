3990dfac20f0618c255eb4892b599677ba29ee99
Luix Lar <Luix@Luixs-MacBook-Pro.local>
2018-06-29 19:39:10 +0200

    Added develop/deploynetserver.sh

---
+++ develop/deploynetserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HOME=/Users/Luix/
+PATHDEV=PycharmProjects/iotelasticproject/
+GIT=git/loraperformance/develop
+FILENAME=iotnetworkserver.py
+
+
+cp "$PATHDEV""$FILENAME" "$PATHDEV""$GIT" 
